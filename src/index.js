1691e34c9214d22aa550030c880d3c64bba23e8c
alopine <loariel98@gmail.com>
2022-05-17 11:03:20 -0700

    Implement hover and click drop-down menus

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import 'normalize.css';
 import './styles.css';
 
+class Events {
+  static listeners() {
+    this.hoverListener();
+    this.clickListener();
+  }
 
+  static hoverListener() {
+    const dropDowns = document.getElementById('dropDownHover').querySelectorAll('.linksContainer');
+    dropDowns.forEach((dropDown) => {
+      const menu = dropDown.parentElement;
+      menu.addEventListener('mouseenter', () => {
+        dropDown.classList.remove('hidden');
+      });
+      menu.addEventListener('mouseleave', () => {
+        dropDown.classList.add('hidden');
+      });
+    });
+  }
+
+  static clickListener() {
+    const dropDowns = document.getElementById('dropDownClick').querySelectorAll('.linksContainer');
+    dropDowns.forEach((dropDown) => {
+      const header = dropDown.previousElementSibling;
+      header.addEventListener('click', () => {
+        dropDown.classList.toggle('hidden');
+      });
+    });
+  }
+}
+
+Events.listeners();
