713d66ac0513eea95868388e9a502d4ec459608e
alopine <loariel98@gmail.com>
2022-05-17 13:41:32 -0700

    Implement mobile menu demo

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ class Events {
   static listeners() {
     this.hoverListener();
     this.clickListener();
+    this.mobileMenuListener();
   }
 
   static hoverListener() {
-    const dropDowns = document.getElementById('dropDownHover').querySelectorAll('.linksContainer');
+    const dropDowns = document
+      .getElementById('dropDownHover')
+      .querySelectorAll('.linksContainer');
     dropDowns.forEach((dropDown) => {
       const menu = dropDown.parentElement;
       menu.addEventListener('mouseenter', () => {
@@ -21,11 +24,39 @@ class Events {
   }
 
   static clickListener() {
-    const dropDowns = document.getElementById('dropDownClick').querySelectorAll('.linksContainer');
+    const dropDowns = document
+      .getElementById('dropDownClick')
+      .querySelectorAll('.linksContainer');
     dropDowns.forEach((dropDown) => {
       const header = dropDown.previousElementSibling;
       header.addEventListener('click', () => {
         dropDown.classList.toggle('hidden');
+      });
+    });
+  }
+
+  static mobileMenuListener() {
+    const menuTabs = {
+      tab1Button: 'tab1Page',
+      tab2Button: 'tab2Page',
+      tab3Button: 'tab3Page',
+      tab4Button: 'tab4Page',
+    };
+    const buttons = document.querySelectorAll('.tabButton');
+    const pages = document.querySelectorAll('.tabPage');
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        if (!button.classList.contains('active')) {
+          const lastActive = document.querySelector('.active');
+          lastActive.classList.remove('active');
+          pages.forEach((page) => {
+            if (!page.classList.contains('hidden')) {
+              page.classList.add('hidden');
+            }
+          });
+          button.classList.add('active');
+          document.getElementById(menuTabs[button.id]).classList.remove('hidden');
+        }
       });
     });
   }
